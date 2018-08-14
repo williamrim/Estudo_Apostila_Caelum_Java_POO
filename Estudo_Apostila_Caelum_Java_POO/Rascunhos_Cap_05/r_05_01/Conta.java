396d3204a04d0715f1a3df9d61112b363e3f92a9
William <41237516+williamrim@users.noreply.github.com>
2018-08-14 20:11:43 -0300

    Rascunho 05_01

--- Estudo_Apostila_Caelum_Java_POO/Rascunhos_Cap_05/r_05_01/Conta.java
+++ Estudo_Apostila_Caelum_Java_POO/Rascunhos_Cap_05/r_05_01/Conta.java
@@ -3,10 +3,22 @@ package r_05_01;
 public class Conta {
 	String titular;
 	int numero;
-	private double saldo;
+	double saldo;
+	//private double saldo;
 	
+	/*
 	void saca(double valor) {
 		this.saldo = this.saldo - valor;
 	}
-
+	*/
+	public void saca(double valor) {
+		// possível sacar até o valor do saldo.
+		if (valor > this.saldo) {
+			System.out.println("Impossível sacar valor maior que o saldo.");
+		}
+		else {
+			this.saldo -= valor;
+		}
+	}
+	
 }
