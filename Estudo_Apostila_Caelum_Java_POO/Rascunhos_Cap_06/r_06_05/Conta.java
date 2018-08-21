088ce71cf7c26816b50b74587d8c14908e890270
William <41237516+williamrim@users.noreply.github.com>
2018-08-21 18:57:15 -0300

    Rascunho 06_06

---
+++ Estudo_Apostila_Caelum_Java_POO/Rascunhos_Cap_06/r_06_05/Conta.java
@@ -0,0 +1,17 @@
+package r_06_05;
+
+public class Conta {
+	
+	private double saldo;
+
+	void deposita(double valor) {
+		this.saldo += valor;
+	}
+
+	public double getSaldo() {
+		// TODO Auto-generated method stub
+		return this.saldo;
+	}
+
+
+}
---
+++ Estudo_Apostila_Caelum_Java_POO/Rascunhos_Cap_06/r_06_05/Principal.java
@@ -0,0 +1,10 @@
+package r_06_05;
+
+public class Principal {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.deposita(100);
+		System.out.println(conta.getSaldo());
+	}
+
+}
