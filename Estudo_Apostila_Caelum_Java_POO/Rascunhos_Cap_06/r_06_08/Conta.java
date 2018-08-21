5cc98e664dbedc81fffb5d97b4400601afe2588c
William <41237516+williamrim@users.noreply.github.com>
2018-08-21 19:56:21 -0300

    Rascunho 06_08

---
+++ Estudo_Apostila_Caelum_Java_POO/Rascunhos_Cap_06/r_06_08/Conta.java
@@ -0,0 +1,37 @@
+package r_06_08;
+
+public class Conta {
+	private String titular;
+	private int numero;
+	private String agencia;
+	private double saldo;
+
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+
+	public void saca(double valor) {
+		this.saldo -= valor;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+}
---
+++ Estudo_Apostila_Caelum_Java_POO/Rascunhos_Cap_06/r_06_08/TestaConta.java
@@ -0,0 +1,9 @@
+package r_06_08;
+
+public class TestaConta {
+	public static void main(String[] args) {
+		Conta c1 = new Conta();
+		System.out.println(c1.getSaldo());
+	}
+
+}
